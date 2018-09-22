5f1d75a52c1e09d35f583896015c4e33940555f0
Bret Walker <bw@bretw.com>
2018-09-22 12:05:40 -0400

    Doc updates and script fixes

--- swap/docker-entrypoint.sh
+++ swap/docker-entrypoint.sh
@@ -3,13 +3,5 @@
 echo "Waiting for database to start"
 ./wait-for-it.sh db:5432 --timeout=60 -- echo "Postgres is up"
 
-echo "Making database migrations"
-./manage.py makemigrations districts noauth
-echo "Applying database migrations"
-./manage.py migrate
-
-echo "Load sample data"
-./manage.py loaddata local.yaml
-
 echo "Starting server"
 ./manage.py runserver 0.0.0.0:8001
