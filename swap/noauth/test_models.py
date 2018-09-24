ce9feca012ea5bb48de4672723052749de24bf3b
Bret Walker <bw@bretw.com>
2018-09-23 21:11:44 -0400

    Working on tests

---
+++ swap/noauth/test_models.py
@@ -0,0 +1,38 @@
+from django.test import TestCase, override_settings
+from django.urls import reverse
+from django.utils.translation import gettext as _
+
+from districts.models import District
+from hamcrest import *
+
+from .models import DEFAULT_CODE_LENGTH, AuthCode, User
+
+
+class AuthCodeModelTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.d = District.objects.create()
+
+    def setUp(self):
+        self.u = User.objects.create(is_active=True, district=AuthCodeModelTests.d)
+
+    def test_create_code_for_user_returns_none_for_inactive_user(self):
+        self.u.is_active = False
+        self.u.save()
+        assert_that(AuthCode.create_code_for_user(self.u), none())
+
+    def test_create_code_for_user_creates_new_code(self):
+        expected_auth_code = AuthCode.create_code_for_user(self.u)
+        actual_auth_code = AuthCode.objects.get(
+            user=self.u, code=expected_auth_code.code
+        )
+        assert_that(expected_auth_code.code, equal_to(actual_auth_code.code))
+
+    def test_generate_code_creates_code_of_default_length(self):
+        code = AuthCode.create_code_for_user(self.u).code
+        assert_that(len(code), equal_to(DEFAULT_CODE_LENGTH))
+
+    @override_settings(NOAUTH_CODE_LENGTH=19)
+    def test_generate_code_creates_code_of_length_defined_in_settings(self):
+        code = AuthCode.create_code_for_user(self.u).code
+        assert_that(len(code), equal_to(19))
--- swap/noauth/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-
-class FirstTest(TestCase):
-    def test_success(self):
-        assert 1 == 1
