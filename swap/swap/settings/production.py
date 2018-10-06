0785579ece0d1fbc7e13f42e726098e715df6c02
Bret Walker <bw@bretw.com>
2018-10-06 14:50:31 -0400

    Heroku

--- swap/swap/settings/production.py
+++ swap/swap/settings/production.py
@@ -10,16 +10,6 @@ from django.core.exceptions import ImproperlyConfigured
 
 from .base import *
 
-
-def get_env_setting(setting):
-    """ Get the environment setting or return exception """
-    try:
-        return environ[setting]
-    except KeyError:
-        error_msg = "Set the %s env variable" % setting
-        raise ImproperlyConfigured(error_msg)
-
-
 DEBUG = False
 
 ########## HOST CONFIGURATION
@@ -65,5 +55,5 @@ CACHES = {}
 
 ########## SECRET CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = get_env_setting("SECRET_KEY")
+SECRET_KEY = get_env_variable("SECRET_KEY")
 ########## END SECRET CONFIGURATION
